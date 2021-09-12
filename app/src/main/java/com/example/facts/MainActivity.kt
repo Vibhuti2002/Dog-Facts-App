4de940ef42fbb713370f1270eb6a0e921a3bb376
vibhuti patil <86654213+Vibhuti2002@users.noreply.github.com>
2021-09-12 09:13:29 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/facts/MainActivity.kt
+++ app/src/main/java/com/example/facts/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
         setFact(index)
     }
     fun setFact(index:Int){
